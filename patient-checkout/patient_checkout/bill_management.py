54781dc8609d5291047f88f746d98a625b683baa
hiepnh <hoanghiepuit97@gmail.com>
2021-12-08 12:01:17 +0700

    [ADD] Patient checkout: handling Logging

--- patient-checkout/patient_checkout/bill_management.py
+++ patient-checkout/patient_checkout/bill_management.py
@@ -1,5 +1,10 @@
+import logging
+
+
+logger = logging.getLogger('patient_checkout')
+logger.setLevel(logging.INFO)
 
 
 def bill_management(event, context):
     message = event['Records'][0]['Sns']['Message']
-    print(message)
+    logger.info(f'******* Message: {message}')
--- patient-checkout/patient_checkout/patient_checkout.py
+++ patient-checkout/patient_checkout/patient_checkout.py
@@ -1,21 +1,26 @@
 import json
 import boto3
 import os
+import logging
+
 
 s3 = boto3.client('s3')
 sns_client = boto3.client('sns')
+logger = logging.getLogger('patient_checkout')
+logger.setLevel(logging.INFO)
 
 
 def push_message_to_SNS(event, context):
     topic = os.environ.get('PATIENT_CHECKOUT_TOPIC')
     bucket_name = event['Records'][0]['s3']['bucket']['name']
     file_key = event['Records'][0]['s3']['object']['key']
-    print(f'******** bucket_name: {bucket_name} -- file_key: {file_key}')
+    logger.info(f'******** Bucket Name: {bucket_name} -- File Key: {file_key}')
     obj = s3.get_object(Bucket=bucket_name, Key=file_key)
     file_content = obj['Body'].read().decode('utf-8')
     checkout_events = json.loads(file_content)
     for event in checkout_events:
-        print(event)
+        logger.info(f'******** Message being published')
+        logger.info(f'******** event: {event}')
         sns_client.publish(
             TopicArn=topic,
             Message=json.dumps({'default': json.dumps(event)}),
